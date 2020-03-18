3c94c5ef620bb94434f7e691bbd91fafac042be5
stefa <stefaniatirlescu@gmail.com>
2020-03-18 19:23:48 +0200

    First commit

---
+++ Tema2/src/ro/ase/csie/cts/g1084/tema2/TestGit.java
@@ -0,0 +1,9 @@
+package ro.ase.csie.cts.g1084.tema2;
+
+public class TestGit {
+
+	public void main() {
+		System.out.println("Hello Git ! Denumirea proiectului de licenta este Planifiacarea si "
+				+ "gestionarea activitatilor turistice utilizand tehnologii mobile");
+	}
+}
